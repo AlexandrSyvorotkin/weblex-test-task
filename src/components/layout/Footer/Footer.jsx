f094f6b71987855efc71b5eed7a780050c2a0cbf
Your Name <you@example.com>
2023-03-10 17:33:17 +0300

    footer ending

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -19,31 +19,37 @@ const links = [
 const Footer = () => {
     return (
         <footer className='footer'>
-            <div className='footer_links'>
-                {footer_links.map(it =>
-                    <div className='footer_link' key={it.id}>
-                        <p className='footer_link_title'>{it.title}</p>
-                        <ul className='footer_list_items'>
-                            {it.links.map(it =>
-                                <li>{it}</li>
-                            )}
-                        </ul>
-                    </div>
-                )}
-            </div>
-            <div className='contacts'>
-                <p className='contacts_title'>Контакты</p>
-                <div className='contact_items'>
-                    <p>+7 555 555-55-55</p>
-                    <div className='links_footer_container'>
-                        {links.map(it =>
-                            <div key={it.id}>
-                                <img src={it.img} alt=""/>
-                            </div>
-                        )}
-                    </div>
+            <div className="footer_wrapper">
+                <div className='footer_links'>
+                    {footer_links.map(it =>
+                        <div className='footer_link' key={it.id}>
+                            <p className='footer_link_title'>{it.title}</p>
+                            <ul className='footer_list_items'>
+                                {it.links.map(it =>
+                                    <li>{it}</li>
+                                )}
+                            </ul>
+                        </div>
+                    )}
                 </div>
-                <p>Москва, Путевой проезд 3с1, к 902</p>
+                <div className='contacts'>
+                    <p className='contacts_title'>Контакты</p>
+                    <div className='contact_items'>
+                        <p>+7 555 555-55-55</p>
+                        <div className='links_footer_container'>
+                            {links.map(it =>
+                                <div key={it.id}>
+                                    <img src={it.img} alt=""/>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                    <p>Москва, Путевой проезд 3с1, к 902</p>
+                </div>
+            </div>
+            <div className='footer_rights'>
+                <p>©WELBEX 2022. Все права защищены.</p>
+                <p>Политика конфиденциальности</p>
             </div>
         </footer>
     );
